befcdc864a88c3174ef2fd65d44143bbfec4f9c8
Abhicoding <shinganeabhishek@gmail.com>
2018-09-21 09:23:00 +0530

    Trying to catch error

--- index.js
+++ index.js
@@ -2,36 +2,66 @@ function Future (fn) {
   var status = 'pending'
   var value
 
-  this.then = function (cbResolve) {
-    return new Future (function (resolve) {
+  this.then = function (cbResolve, cbReject) {
+    return new Future (function (resolve, reject) {
       handle({
         cbResolve,
-        resolve
+        resolve,
+        cbReject,
+        reject
       })
     })
   }
 
   function resolve (input) {
-    if(input && input.then === 'function'){
-      input.then(resolve)
-      return
+    try {
+      if(input && input.then === 'function'){
+        input.then(resolve)
+        return
+      }
+      
+      status = 'resolved'
+      value = input
+    } catch (e) {
+      reject(e)
     }
-    status = 'resolved'
-    value = input
+  }
+
+  function reject (reason) {
+    status = 'rejected'
+    value = reason
   }
 
   function handle (handler) {
+    // console.log(handler)
 
-    if (!handler.cbResolve) {
-      handler.resolve(value)
-      return
+    var handleCallback
+    
+    if (status === 'resolved') {
+      handleCallback = handler.cbResolve
+    } else {
+      handleCallback = handle.cbReject
     }
 
-    var returnValue = handler.cbResolve(value)
-    return handler.resolve(returnValue)
+    if (!handleCallback) {
+      if (status === 'resolved') {
+          handler.resolve(value);
+      } else {
+          handler.reject(value);
+      }
+      return;
   }
 
-  fn (resolve)
+    var returnValue
+    try {
+      returnValue = handler.cbResolve(value)
+      handler.resolve(returnValue)
+    } catch (e) {
+      handler.reject(e)
+    }
+  }
+
+  fn (resolve, reject)
 }
 
 function doSomething (value) {
@@ -41,16 +71,16 @@ function doSomething (value) {
   })
 }
 
-doSomething(21)
- .then(x => {
-    x = (x+1) * 2
-    console.log(x)
-    return x
-  })
-  .then(x => {
-    x = (x+1) * 2
-    console.log(x)
-  return x})
+// doSomething(21)
+//  .then(x => {
+//     x = (x+1) * 2
+//     console.log(x)
+//     return x
+//   })
+//   .then(x => {
+//     x = (x+1) * 2
+//     console.log(x)
+//   return x})
 //   .then(x => {
 //     console.log(2*x--)
 //     return 2 * x--
@@ -59,17 +89,29 @@ doSomething(21)
 //     console.log(x/2)
 //   })
 
-  // function getSomeJson () {
-  //   return new Promise (function (resolve, reject){
-  //     var badJson = "<div> uh oh, this is not JSON at all </div>"
-  //     resolve(badJson)
-  //   })
-  // }
+function getSomeJson() {
+  return new Future(function(resolve, reject) {
+    var badJson = "<div>uh oh, this is not JSON at all!</div>";
+    resolve(badJson);
+  });
+}
 
-  // getSomeJson()
-  //   .then(function badJsonCb(json){
-  //     var obj = JSON.parse(json)
-  //     console.log(json)
-  //   }, function badJsonErr  (error) {
-  //     console.log('uh oh', error)
-  //   })
+// getSomeJson().then(function(json) {
+//   var obj = JSON.parse(json);
+//   console.log(obj);
+// }, function(error) {
+//   console.log('uh oh', error);
+// });
+
+getSomeJson()
+  .then(function(json) {
+    var obj = JSON.parse(json)
+    console.log(obj)})
+  .then(null, function(error) {
+    console.log("an error occured: ", error)})
+// var badJson = "<div>uh oh, this is not JSON at all!</div>"
+// try {
+//   JSON.parse(badJson)
+// } catch (e) {
+//   console.log('Hurr Durr. Bad JSON', e.name)
+// }
